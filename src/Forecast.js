23e6cdb98c7488d68d9e3d2ecc44245461323219
Angry-cat-coder <y.v.isaenko@gmail.com>
2022-12-02 22:40:55 +0200

    created states for forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forecast.css";
 import Weathericon from "./Weathericon";
 import axios from "axios";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecastDate, setForecastDate] = useState({});
+
   function showResponse(response) {
     console.log(response.data);
+    setLoaded(true);
+    setForecastDate(response.data.daily);
   }
-  //function searChing() {
-  let lat = props.coords.lat;
-  let lon = props.coords.lon;
-  let apiKey = "712149961fc69177944e721143a58b42";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showResponse);
-  // }
 
+  if (loaded === false) {
+    let lat = props.coords.lat;
+    let lon = props.coords.lon;
+    let apiKey = "712149961fc69177944e721143a58b42";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showResponse);
+    return null;
+  } else console.log(forecastDate);
   return (
     <div className="Forecast">
       <div className="row">
@@ -28,7 +34,7 @@ export default function Forecast(props) {
       </div>
       <div className="row Picture">
         <div className="col-2">
-          <Weathericon codeicon={"01d"} size={30} />{" "}
+          <Weathericon codeicon={forecastDate[0].weather[0].icon} size={30} />{" "}
         </div>
         <div className="col-2">🌦 </div>
         <div className="col-2">🌨 </div>
@@ -38,7 +44,8 @@ export default function Forecast(props) {
       </div>
       <div className="row Temp">
         <div className="col-2">
-          <span className="Day">16 </span>12
+          <span className="Day">{forecastDate[0].temp.max}</span>
+          {forecastDate[0].temp.min}
         </div>
         <div className="col-2">
           <span className="Day">16 </span>12
